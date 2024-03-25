8b47f99d3c8740370d26990a8dfdc4321ac57ed7
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-03-25 11:21:43 +0100

    feat: add test get teacher by name

--- src/test/scala/tasks/adts/SchoolTests.scala
+++ src/test/scala/tasks/adts/SchoolTests.scala
@@ -8,9 +8,9 @@ import u03.Optionals.*
 import u03.Sequences.Sequence
 
 class SchoolTests {
-  val schoolADT = SchoolADT
+  val schoolADT: Ex2SchoolModel.SchoolADT.type = SchoolADT
   import schoolADT.*
-  val generalSchool = school(Nil(), Nil())
+  val generalSchool: School = school(Nil(), Nil())
 
   @Test def testAddTeacher(): Unit =
     val t: Teacher = teacher("mario")
@@ -22,5 +22,9 @@ class SchoolTests {
     val expected: School = school(Nil(), Cons(c, Nil()))
     assertEquals(expected, generalSchool.addCourse("pps"))
 
+  @Test def testGetTeacherByName(): Unit =
+    val t = teacher("mario")
+    val school = generalSchool.addTeacher("mario")
+    assertEquals(Optional.Just(t), school.teacherByName("mario"))
 
 }
