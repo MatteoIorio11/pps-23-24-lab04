ef850fd94e9e729d504c467e9b7a98fc6f6a2eb0
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-03-25 12:58:34 +0100

    feat: add testPush

--- src/test/scala/tasks/adts/StackTests.scala
+++ src/test/scala/tasks/adts/StackTests.scala
@@ -1,5 +1,14 @@
 package tasks.adts
 
-class StackTests {
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import tasks.adts.Ex3Stacks.{StackADT, StackImpl}
 
+class StackTests {
+  val stackADT: StackADT = StackImpl
+  import stackADT.*
+  val myStack: Stack[Int] = stackADT.empty
+
+  @Test def testPush(): Unit =
+    assertEquals(empty.push(1), myStack.push(1))
 }
