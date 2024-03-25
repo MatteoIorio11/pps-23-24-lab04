603bec69eefb9c90d406be2a7d58c1923fc6e404
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-03-25 16:29:13 +0100

    feat: add testAsSequence

--- src/test/scala/tasks/adts/StackTests.scala
+++ src/test/scala/tasks/adts/StackTests.scala
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import tasks.adts.Ex3Stacks.{StackADT, StackImpl}
 import u03.Optionals.Optional
+import u03.Sequences.Sequence.*
 
 class StackTests {
   val stackADT: StackADT = StackImpl
@@ -17,4 +18,8 @@ class StackTests {
     val stack = myStack.push(1).push(2).push(3)
     val opt = stack.pop(3)
     assertEquals(Optional.Just((3, empty.push(1).push(2))), opt)
+
+  @Test def testAsSequence(): Unit =
+    val stack = myStack.push(1).push(2).push(3)
+    assertEquals(Cons(3, Cons(2, Cons(1, Nil()))), stack.asSequence())
 }
