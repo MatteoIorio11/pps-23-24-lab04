6c3bcad64de4426ad38e29f6935068d76e1d5d4b
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-03-25 13:05:12 +0100

    feat: add testPop

--- src/test/scala/tasks/adts/StackTests.scala
+++ src/test/scala/tasks/adts/StackTests.scala
@@ -3,6 +3,7 @@ package tasks.adts
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import tasks.adts.Ex3Stacks.{StackADT, StackImpl}
+import u03.Optionals.Optional
 
 class StackTests {
   val stackADT: StackADT = StackImpl
@@ -11,4 +12,9 @@ class StackTests {
 
   @Test def testPush(): Unit =
     assertEquals(empty.push(1), myStack.push(1))
+
+  @Test def testPop(): Unit =
+    val stack = myStack.push(1).push(2).push(3)
+    val opt = stack.pop(3)
+    assertEquals(Optional.Just((3, empty.push(1).push(2))), opt)
 }
