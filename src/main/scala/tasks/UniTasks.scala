c2255056fcd9b654319c3715fc8433b918771080
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-01 11:36:44 +0200

    feat: add task7

--- src/main/scala/tasks/UniTasks.scala
+++ src/main/scala/tasks/UniTasks.scala
@@ -13,6 +13,8 @@ import u03.Sequences.Sequence.Nil
 import u04lab.Ex4Summables.Summable
 import u03.extensionmethods.Optionals.Optional.None
 import u04.monads.Monads.Monad
+import u04.monads.States.State
+import u04.monads.{CounterStateImpl, Monads, States, WindowStateImpl}
 
 import scala.annotation.tailrec
 
@@ -169,3 +171,39 @@ object UniTasks:
         override def flatMap[B](f: A => Try[B]): Try[B] = m match
           case TryImpl.Success(value) => f(value)
           case TryImpl.Failure(exception) => TryImpl.Failure(exception)
+
+  object Task7:
+    def runMVC =
+      import Monads.*, Monad.*, States.*, State.*, CounterStateImpl.*, WindowStateImpl.*
+      import u03.extensionmethods.Streams.*
+
+      def mv[SM, SV, AM, AV](m1: State[SM, AM], f: AM => State[SV, AV]): State[(SM, SV), AV] =
+        State: (sm, sv) =>
+          val (sm2, am) = m1.run(sm)
+          val (sv2, av) = f(am).run(sv)
+          ((sm2, sv2), av)
+
+      def windowCreation(str: String): State[Window, Stream[String]] = for
+        _ <- setSize(300, 300)
+        _ <- addButton(text = "inc", name = "IncButton")
+        _ <- addButton(text = "dec", name = "DecButton")
+        _ <- addButton(text = "reset", name = "ResetButton")
+        _ <- addButton(text = "quit", name = "QuitButton")
+        _ <- addButton(text = "set", name = "SetButton")
+        _ <- addTextField("TextField")
+        _ <- addLabel(text = str, name = "Label1")
+        _ <- show()
+        events <- eventStream()
+      yield events
+
+      val controller = for
+        events <- mv(seq(reset(), get()), i => windowCreation(i.toString()))
+        _ <- seqN(events.map(_ match
+          case "SetButton" => mv(nop(), i => toLabelFromTextField("TextField", "Label1"))
+          case "IncButton" => mv(seq(inc(), get()), i => toLabel(i.toString, "Label1"))
+          case "DecButton" => mv(seq(dec(), get()), i => toLabel(i.toString, "Label1"))
+          case "ResetButton" => mv(seq(reset(), get()), i => toLabel(i.toString, "Label1"))
+          case "QuitButton" => mv(nop(), _ => exec(sys.exit()))))
+      yield ()
+
+      controller.run((initialCounter(), initialWindow))
